af8474114b7bb9f1dba626f297228db286c8b20a
varutcha <varutcha.c@rmutsv.ac.th>
2024-01-22 11:24:31 +0700

    deploy

---
+++ src/components/ClassRoomList.jsx
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+
+const ClassRoomList = () => {
+
+    const [classRoom, setClassRoom] = useState([])
+    const token = localStorage.getItem('token');
+
+    useEffect(() => {
+        // Check if the user is logged in
+        if (!token) {
+            window.location.href = '/login'
+        } else {
+            const apiEndpoint = `${import.meta.env.VITE_API_URL}/teacher/supervisor/${token}`;
+            // const token = localStorage.getItem('token');
+            const fetchData = async () => {
+                const response = await fetch(apiEndpoint, {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${token}`
+                    }
+                });
+                const data = await response.json();
+                console.log(data);
+                setClassRoom(data.class)
+            }
+            fetchData();
+
+        }
+    }
+        , [token]);
+
+
+
+
+    return (
+        <div className='container'>
+            <p className='display-6'>รายชื่อห้องที่ปรึกษา</p>
+
+            <div className="row">
+                {classRoom.map((classroom, index) => (
+
+                    <div className="col mx-auto" key={index}>
+
+                        <div className="card">
+                            <div className="card-body">
+                                <h5 className="card-title">{classroom.classname}</h5>
+
+                                <Link to={`/members/${classroom.classid}`} className="btn btn-primary">เข้าสู่ห้องเรียน</Link>
+                            </div>
+                        </div>
+                    </div>
+
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default ClassRoomList
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className="sticky text-center">
+            <p >&copy; Copyright 2024 สำนักวิทยบริการและเทคโนโลยีสารสนเทศ</p>
+
+        </div>
+    )
+}
+
+export default Footer
